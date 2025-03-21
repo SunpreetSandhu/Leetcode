ddeb2a04845eba4d624197cd7ce48e764c6e284e
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2025-03-21 11:43:38 -0400

    top k

--- 01_Arrays_&_Hashing/13_top_k_frequent_elements.py
+++ 01_Arrays_&_Hashing/13_top_k_frequent_elements.py
@@ -1,20 +1,20 @@
 '''
-Intuition: 
-We need to find k most frequent elements given an array nums and we need an efficient way to get the k elements with highest frequency. We can use a BUCKET SORT inspired approach where we group the elements based on their frequency, for example index 6 in the bucket will correspond to the element that appears 6 times.After we have this bucket, we can loop backwards and extract the k elements
+Intuition:
+The goal is to find the k most frequent elements in an array. The naive approach involves counting frequencies and then sorting the elements by frequency and selecting the top k. The sorting takes O(nlogn) time, and we can instead do a bucket sort approach in O(n) time by leveraging the frequency as an index, avoiding the sorting.
 
 Approach:
-1)Count the occurance of each element using hashmap with key = num, val = freq
-2)Initialize buckets array to be initially empty array for each index, and we will initialize len(nums)+1 indexes, since we want the index of the bucket to correspond to the freq. Loop through the hashmap.
-    a) Store the hashmap's freq at that particular key, and append the key to index in buckets corresponding to freq
-3)Initialize result array initally empty, and loop backwards from len(buckets)-1, (since if buckets is length 7, buckets[7] would be out of bounds so we need to do -1).
-    i)If there is a value at buckets[i], we will EXTEND the reusult array to add all the values that are in that frequency. We will then check if k elements have been reached, if so we return result up to k elements.
-4) Return resu
+1) Count Frequencies:
+    a)Use a hashmap to count the frequency of each element in nums.
+2) Bucket Sort by Frequency:
+    a)Create a list of buckets where the index represents the exact frequency   (which is why we need len(nums) +1 buckets). Bucket i will store elements that appear i times, and elements will appear at most len(nums) times.
 
-Time complexity: O(n) - O(n) to iterate through nums, O(m) to iterate through hm where m is unique elements, O(n) to iterate through buckets in the worst case, overall O(n+1)
+3) Extract top k elements:
+    a)Iterate from the top of the buckets array down, and collect elements until the result contains k or more elements. When this happens we will print everything from the array not including the Kth element if its present
 
-Space Complexity: O(n) - O(m) for hm in worst case, O(n+1) for buckets, O(k) for resu. Overall O(m+n+1+k), ie O(n)
+Time Complexity: O(n) -> Counting frequencies: O(n), Building buckets: O(n) (each unique element is processed once), Extracting top k: O(n) (worst case, all elements have unique frequencies)
+
+Space Complexity: O(n) -> Worst case, all elements are unique and hash maps scales the input array, and also the bucket is O(n)
 '''
-
 class Solution(object):
     def topKFrequent(self, nums, k):
         """
@@ -22,22 +22,28 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        hm = {}
+
+        hm ={}
         for num in nums:
             if num not in hm:
                 hm[num] = 1
             else:
                 hm[num] +=1
-        buckets = [[] for i in range(len(nums)+1)]
+        
+        bucket = [[] for i in range(len(nums) +1)]
         for key in hm:
-            freq = hm[key]
-            buckets[freq].append(key)
+            bucket[hm[key]].append(key)
+
         resu = []
-        for i in range(len(buckets)-1, 0, -1):
-            if buckets[i]:
-                resu.extend(buckets[i])
+        for i in range(len(bucket)-1, 0, -1):
+            if bucket[i]:
+                resu.extend(bucket[i])
             if len(resu)>=k:
                 return resu[:k]
         return resu
+
+
+
+        
 
         
